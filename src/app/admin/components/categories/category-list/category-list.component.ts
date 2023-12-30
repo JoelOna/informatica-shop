cea20907cea913cdab8180e60dad8895084211db
JoelOna <onaferrerjoel@gmail.com>
2023-12-30 17:07:20 +0100

    category list

---
+++ src/app/admin/components/categories/category-list/category-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ICategory } from 'src/app/interface/icategory';
+import { CategoryDataService } from 'src/app/services/category-data.service';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss']
+})
+export class CategoryListComponent implements OnInit{
+  constructor(private category_service:CategoryDataService){}
+  categories: ICategory[] = []
+
+  ngOnInit(): void {
+    this.getCategories()
+  }
+
+  private getCategories(): void{
+    this.category_service.getCategories().subscribe(
+      resp =>{
+        if (resp.body) {
+          this.categories = resp.body.data
+        }
+      },
+      error=>{
+
+      }
+    )
+  }
+}
