0c5a97d979b3d63c4d3b5c88294d3464f5d6ff38
JoelOna <onaferrerjoel@gmail.com>
2024-01-09 20:14:33 +0100

    shopping-cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductComponent } from './components/product/product.component';
-import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ShopProcesComponent } from './components/shop-proces/shop-proces.component';
 
 const routes: Routes = [
-  { path: 'ifshop-admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminRoutingModule) },
+  { path: 'ifshop-admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminRoutingModule), title: 'Admin | Informatica Shop' },
   {path:'', component: HomeComponent,title: 'Inicio | Informatica Shop'},
   {path:'products', component: ProductsComponent,title: 'Productos | Informatica Shop'},
   {path:'product/:id', component: ProductComponent},
+  {path:'login2', component: LoginComponentComponent, title: 'Inicio sesión | Informatica Shop'},
   {path:'login', component: LoginComponent, title: 'Inicio sesión | Informatica Shop'},
-  {path:'signup', component:SignupComponent, title: 'Registro | Informatica Shop'}
+  {path:'signup', component:SignupComponent, title: 'Registro | Informatica Shop'},
+  {path:'carrito', component:CartComponent, title: 'Carrito | Informatica Shop'},
+  {path:'proceso-compra', component:ShopProcesComponent, title: 'Proceso de comprar | Informatica Shop'}
 ];
 
 @NgModule({
